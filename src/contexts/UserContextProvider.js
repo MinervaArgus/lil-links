c5a9d1027400bc4ef2ede84eb035304388467a3e
MinervaArgus <71613767+MinervaArgus@users.noreply.github.com>
2023-04-23 17:09:27 -0400

    updated

--- src/contexts/UserContextProvider.js
+++ src/contexts/UserContextProvider.js
@@ -1,35 +1,34 @@
-import { createContext, useContext, useState, useEffect } from 'react';
+import { useEffect, useState, createContext, useContext } from 'react';
+import supabase from '../lib/supabase';
 
 const UserContext = createContext();
 
-export const UserContextProvider = ({ supabaseClient, children }) => {
+export const useUser = () => useContext(UserContext);
+
+export function UserContextProvider({ children }) {
   const [session, setSession] = useState(null);
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const session = supabaseClient.auth.session();
+    const session = supabase.auth.session();
     setSession(session);
     setUser(session?.user ?? null);
 
-    const { data: authListener } = supabaseClient.auth.onAuthStateChange(async (event, session) => {
-      setSession(session);
-      setUser(session?.user ?? null);
-    });
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        setSession(session);
+        setUser(session?.user ?? null);
+      }
+    );
 
     return () => {
       authListener.unsubscribe();
     };
   }, []);
 
-  const value = {
-    session,
-    user,
-    supabaseClient,
-  };
-
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
-};
-
-export const useUser = () => {
-  return useContext(UserContext);
-};
+  return (
+    <UserContext.Provider value={{ user, session }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
